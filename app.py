4f8b32c696e87d5a2bf20bff3188e1c0b5fea5c5
rbtessier <r.b.tessier@gmail.com>
2023-05-28 08:33:46 -0600

    turned off debug

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ server = app.server  # Expose the server variable for deployments.
 app.layout = html.Div('Hello, world!')
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
